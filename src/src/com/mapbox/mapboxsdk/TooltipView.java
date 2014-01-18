6a2b0f662e0af6a068fdc9fb0f7d4ea499f92bf2
Francisco Dans <fdansv@gmail.com>
2014-01-18 19:51:00 +0000

    adds padding setting for tooltip depending on projection. Scroller should do the rest

--- src/src/com/mapbox/mapboxsdk/TooltipView.java
+++ src/src/com/mapbox/mapboxsdk/TooltipView.java
@@ -3,6 +3,7 @@ package com.mapbox.mapboxsdk;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Point;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.view.Gravity;
@@ -17,11 +18,14 @@ import org.osmdroid.util.GeoPoint;
  */
 public class TooltipView extends LinearLayout {
 
+    private final MapView mapView;
     private TextView defaultTextView;
+    private Point location;
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     public TooltipView(Context context, MapView mapView) {
         super(context);
+        this.mapView = mapView;
         mapView.addView(this);
         this.getLayoutParams().width = 400;
         this.getLayoutParams().height = 100;
@@ -40,11 +44,17 @@ public class TooltipView extends LinearLayout {
 
     }
     public void setPosition(GeoPoint point){
-
+        calculatePoint(point);
+        this.setPadding(location.x, location.y, 0, 0);
     }
 
     public void reconfigure(GeoPoint point, String title) {
         setText(title);
         setPosition(point);
+    }
+
+    private void calculatePoint(GeoPoint point){
+        MapView.Projection projection = mapView.getProjection();
+        projection.toPixels(point, location);
     }
 }
